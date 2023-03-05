53124fec4c8165e52ff584619a0b1676334875bb
shivam urkude <shivamurkude2000@gmail.com>
2023-03-05 13:11:25 +0530

    Profile models and signals created

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class TimeStampedUUIDModel(models.Model):
+    pkid=models.BigAutoField(primary_key=True, editable=False)
+    id=models.UUIDField(unique=True,editable=False,default=uuid.uuid4)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
